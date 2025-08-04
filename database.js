51d603768313935b68c22830ebda69fbc6d26ddb
Mudit-Tarway <official.mudittarway@gmail.com>
2025-08-04 10:36:13 +0530

    Dtabase logic of the website

---
+++ database.js
@@ -0,0 +1,49 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+const ObjectId = mongoose.Types.ObjectId;
+
+const userSchema = new Schema({
+  email: { type: String, unique: true },
+  password: String,
+  firstName: String,
+  lastName: String,
+});
+
+const courseSchema = new Schema({
+  title: String,
+  description: String,
+  price: Number,
+  creatorId: ObjectId,
+  imageUrl: String,
+});
+
+const adminSchema = new Schema({
+  email: { type: String, unique: true },
+  password: String,
+  firstName: String,
+  lastName: String,
+});
+
+const purchaseSchema = new Schema({
+  userId: ObjectId,
+  courseId: ObjectId,
+});
+
+const usersModel = mongoose.model("users", userSchema);
+const courseModel = mongoose.model("course", courseSchema);
+const adminModel = mongoose.model("admin", adminSchema);
+const purchaseModel = mongoose.model("purchase", purchaseSchema);
+
+mongoose.connect(process.env.MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+.then(() => console.log("Connected to MongoDB"))
+.catch((err) => console.error("MongoDB connection error:", err));
+
+module.exports = {
+  usersModel,
+  courseModel,
+  adminModel,
+  purchaseModel,
+};
